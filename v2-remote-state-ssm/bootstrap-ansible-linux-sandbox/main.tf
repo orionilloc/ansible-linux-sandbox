f665b8fb4b8388ecc4a79db2d7b782166016d55d
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-04-12 18:53:28 -0400

    Scaffolding for roles and playbooks

--- v2-remote-state-ssm/bootstrap-ansible-linux-sandbox/main.tf
+++ v2-remote-state-ssm/bootstrap-ansible-linux-sandbox/main.tf
@@ -4,7 +4,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# The S3 bucket for state management
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "ansible-linux-sandbox-terraform-state"
   lifecycle {
@@ -12,7 +11,6 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-# Enable S3 bucket versioning
 resource "aws_s3_bucket_versioning" "enabled" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
@@ -20,7 +18,6 @@ resource "aws_s3_bucket_versioning" "enabled" {
   }
 }
 
-# The DynamoDB table for locking Terraform state
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-state-locking"
   billing_mode = "PAY_PER_REQUEST"
--- v2-remote-state-ssm/user-data.sh
+++ v2-remote-state-ssm/user-data.sh
@@ -4,12 +4,18 @@ set -euo pipefail
 exec > >(tee /var/log/user-data.log) 2>&1
 
 sudo dnf update -y
-sudo dnf install -y python3 python3-pip awscli
+sudo dnf install -y python3 python3-pip awscli git
 
 sudo pip3 install ansible boto3 --no-cache-dir
 
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
 sudo dnf install -y session-manager-plugin.rpm
+
+git clone --no-checkout https://github.com/orionilloc/ansible-linux-sandbox.git /home/ec2-user
+cd /home/ec2-user
+git sparse-checkout init
+git sparse-checkout set playbooks/ roles/
+git checkout main
 
 cat <<'EOF' > /home/ec2-user/inventory.ini
 ${inventory_content}
@@ -19,7 +25,7 @@ cat <<'EOF' > /home/ec2-user/ansible.cfg
 ${ansible_configuration}
 EOF
 
-chown ec2-user:ec2-user /home/ec2-user/inventory.ini /home/ec2-user/ansible.cfg
+chown -R ec2-user:ec2-user /home/ec2-user/inventory.ini /home/ec2-user/ansible.cfg
 
 ansible --version && boto3_check=$(python3 -c "import boto3" 2>&1) \
   && touch /home/ec2-user/.ansible_setup_complete \

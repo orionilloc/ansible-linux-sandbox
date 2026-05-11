914ef4297761ff2aeb50a2eff39916c33a1c42ed
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-05-10 23:11:55 -0400

    Scaffolding for dynamic inventory with addition of ec2 plugin files and reworking of main.tf and user-data.sh- work in progress!

---
+++ v3-dynamic-inventory/bootstrap-ansible-linux-sandbox/main.tf
@@ -0,0 +1,19 @@
+# main.tf
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "ansible-linux-sandbox-terraform-state"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
